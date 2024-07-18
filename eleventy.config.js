b10a13950bcf2c0686a85e167409427e7b92163d
Ndimah Mike <ndimah22@protonmail.com>
2024-07-18 18:37:20 +0100

    feat(UI): implement the app main menu

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,26 @@
 /*jslint node*/
+const {transform} = require("lightningcss");
+const {readFile} = require("node:fs");
+const {promisify} = require("node:util");
+const path = require("node:path");
+const fileReader = promisify(readFile);
+
+async function parseCss({dir}, src) {
+    const blob = await fileReader(path.normalize(
+        `${dir.input}/${dir.includes}/${src}`
+    ));
+    const {code} = transform({
+        code: blob,
+        filename: "style.css",
+        minify: true
+    });
+    return code.toString();
+}
 module.exports = function (config) {
     config.addPassthroughCopy("assets");
+    config.addShortcode("cssmin", function (src) {
+        return parseCss(config, src);
+    });
     return {
         dir: {
             includes: "_templates",
