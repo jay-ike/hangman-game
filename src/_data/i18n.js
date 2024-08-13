27d97d9341307e5a6b6fc4814017f6a2dc4fed79
Ndimah Mike <ndimah22@protonmail.com>
2024-08-13 12:47:58 +0100

    feat(i18n): internationalize the playing page

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -1,15 +1,52 @@
+/*jslint node*/
 module.exports = Object.freeze({
     available_categories: {
         en: "available categories",
         fr: "catégories disponibles"
     },
-    lang_switch: {
-        en: "change the language",
-        fr: "changer de langue"
+    available_letters: {
+        en: "available letters",
+        fr: "lettres disponibles"
+    },
+    category_loading: {
+        en: "loading category",
+        fr: "chargement de la catégorie"
+    },
+    continue: {
+        en: "continue",
+        fr: "Reprendre"
+    },
+    hearts_left: {
+        en: "hearts remaining",
+        fr: "vies restantes"
+    },
+    hearts_remaining: {
+        en: "you have {hearts} hearts remaining",
+        fr: "Il vous reste {hearts} vies"
     },
     how_to_play: {
         en: "how to play",
         fr: "comment jouer"
+    },
+    lang_switch: {
+        en: "change the language",
+        fr: "changer de langue"
+    },
+    letter_selected: {
+        en: "you have already selected the letter {{letter}}",
+        fr: "la lettre {{letter}} a déjà été selectionnée"
+    },
+    lost: {
+        en: "you lose",
+        fr: "Perdu !!!"
+    },
+    new_category: {
+        en: "switch category",
+        fr: "autre catégorie"
+    },
+    paused: {
+        en: "Paused",
+        fr: "en pause"
     },
     pick_category: {
         en: "pick a category",
@@ -19,8 +56,20 @@ module.exports = Object.freeze({
         en: "play {{name}} now",
         fr: "jouer à {{name}} maintenant"
     },
+    quit_game: {
+        en: "quit game",
+        fr: "quitter le jeu"
+    },
+    replay: {
+        en: "play again",
+        fr: "Rejouer"
+    },
     return_home: {
         en: "return to main menu",
         fr: "retourner au menu principal"
+    },
+    won: {
+        en: "you win",
+        fr: "Trouvé !!!"
     }
 });
