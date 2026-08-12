76f625bdcf706797ea1393b967fc463a187022bd
Ndimah Tchougoua <ndimah22@protonmail.com>
2026-08-12 14:24:12 +0100

    fix: solve the rendering issues on the how to play page
    The cards on the "how to play" page where too narrow which lead to an
    awkward rendering.
    The problem was fixed by setting variable for rule card's container to
    handle the layout

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -1,10 +1,7 @@
 /*jslint node*/
 const link = "<a href='mailto:ndimah22@protonmail.com'>ndimah22@protonmail</a>";
 module.exports = Object.freeze({
-    achievement_logs: {
-        en: "achievement logs",
-        fr: "historique"
-    },
+    achievement_logs: {en: "achievement logs", fr: "historique"},
     available_categories: {
         en: "available categories",
         fr: "catégories disponibles"
@@ -40,6 +37,7 @@ module.exports = Object.freeze({
             en: "Illustrations: Made in Inkscape, tweaked with love.",
             fr: "Illustrations : Dessinées sous Inkscape, ajustées avec amour."
         },
+        name: {en: "credits", fr: "crédits"},
         report: {en: "Report a bug", fr: "Signaler un bug"},
         sound: {
             en: "Sounds: Sourced from Pixabay (royalty‑free).",
@@ -52,34 +50,15 @@ module.exports = Object.freeze({
         title: {en: "behind the words", fr: "derrière les mots"},
         tools: {en: "Tech & Tools", fr: "Technos & Outils"}
     },
-    deduct_points: {
-        en: "deduct points",
-        fr: "débiter mes points"
-    },
-    game_rules: {
-        en: "Gameplay rules",
-        fr: "Règles du jeu"
-    },
-    letter_reveal_unavailable: {
-        en: "You need at least 2 hearts to reveal a letter.",
-        fr: "Tu as besoin d’au moins 2 cœurs pour révéler une lettre."
-    },
-    hearts_left: {
-        en: "hearts remaining",
-        fr: "vies restantes"
-    },
+    deduct_points: {en: "deduct points", fr: "débiter mes points"},
+    game_rules: {en: "Gameplay rules", fr: "Règles du jeu"},
+    hearts_left: {en: "hearts remaining", fr: "vies restantes"},
     hearts_remaining: {
         en: "you have {hearts} hearts remaining",
         fr: "Il vous reste {hearts} vies"
     },
-    hidden_item_category: {
-        en: "item to guess",
-        fr: "élément à deviner"
-    },
-    how_to_play: {
-        en: "how to play",
-        fr: "comment jouer"
-    },
+    hidden_item_category: {en: "item to guess", fr: "élément à deviner"},
+    how_to_play: {en: "how to play", fr: "comment jouer"},
     js_error_content: {
         en: "It seems that scripting is not enabled. If you you have disabled" +
         " Javascript, please consider enabling it. If not consider reporting " +
@@ -88,14 +67,12 @@ module.exports = Object.freeze({
         "vous l'avez désactivé penser à l'activer au cas contraire " +
         "reportez l'érreur À l'adresse: " + link
     },
-    key: {
-        en: "key",
-        fr: "touche"
+    key: {en: "key", fr: "touche"},
+    letter_reveal_unavailable: {
+        en: "You need at least 2 hearts to reveal a letter.",
+        fr: "Tu as besoin d’au moins 2 cœurs pour révéler une lettre."
     },
-    lang_switch: {
-        en: "change the language",
-        fr: "changer de langue"
-    },
+    lang_switch: {en: "change the language", fr: "changer de langue"},
     letter_reveal: {
         en: "reveal a letter in the hidden item",
         fr: "dévoiler une lettre de l'élement masqué"
@@ -104,50 +81,20 @@ module.exports = Object.freeze({
         en: "you have already selected the letter {{letter}}",
         fr: "la lettre {{letter}} est déjà selectionnée"
     },
-    level: {
-        en: "level",
-        fr: "niveau"
-    },
-    level_up: {
-        en: "level up !",
-        fr: "tu avances !"
-    },
-    level_selection: {
-        en: "Pick your difficulty",
-        fr: "Choisis ta difficulté"
-    },
-    loading_failed: {
-        en: "failed to load",
-        fr: "n'a pas pu être chargé"
-    },
-    locked: {
-        en: "unlock to play",
-        fr: "débloque pour jouer"
-    },
-    lost: {
-        en: "so close !",
-        fr: "Presque !"
-    },
-    new_category: {
-        en: "change category",
-        fr: "autre catégorie"
-    },
-    paused: {
-        en: "Paused",
-        fr: "en pause"
-    },
-    perfect: {
-        en: "perfect !!",
-        fr: "parfait !!"
-    },
-    pick_category: {
-        en: "pick your category",
-        fr: "choisis ta catégorie"
-    },
-    play_now: {
-        en: "play {{name}} now",
-        fr: "jouer à {{name}} maintenant"
-    },
+    level: {en: "level", fr: "niveau"},
+    level_up: {en: "level up !", fr: "tu avances !"},
+    level_selection: {en: "Pick your difficulty", fr: "Choisis ta difficulté"},
+    loading_failed: {en: "failed to load", fr: "n'a pas pu être chargé"},
+    locked: {en: "unlock to play", fr: "débloque pour jouer"},
+    lost: {en: "so close !", fr: "Presque !"},
+    my_badges:{en: "my badges", fr: "mes badges"},
+    more: {en: "more options", fr: "plus d'options"},
+    new_category: {en: "change category", fr: "autre catégorie"},
+    other_links: {en: "useful links", fr: "liens utiles"},
+    paused: {en: "Paused", fr: "en pause"},
+    perfect: {en: "perfect !!", fr: "parfait !!"},
+    pick_category: {en: "pick your category", fr: "choisis ta catégorie"},
+    play_now: {en: "play {{name}} now", fr: "jouer à {{name}} maintenant"},
     point_deduction_warning: {
         en: "You are going to loose 2 hearts if you want a letter to be" +
         " revealed. Is it really your intent ?<br>(Note: this action can " +
@@ -156,40 +103,14 @@ module.exports = Object.freeze({
         "soit dévoilée. Est-ce vraiment votre intention ?<br>" +
         "(Note: cette action est irréversible)"
     },
-    quit_game: {
-        en: "quit game",
-        fr: "quitter le jeu"
-    },
-    replay: {
-        en: "Next puzzle",
-        fr: "Prochaine Énigme"
-    },
-    return_category: {
-        en: "change category",
-        fr: "changer de catégorie"
-    },
-    return_home: {
-        en: "return to main menu",
-        fr: "retourner au menu principal"
-    },
-    reveal: {
-        en: "reveal",
-        fr: "devoiler"
-    },
-    reveal_answer: {
-        en: "reveal answer",
-        fr: "dévoiler la réponse"
-    },
-    start: {
-        en: "play now",
-        fr: "lance-toi !"
-    },
-    warning: {
-        en: "caution !!!",
-        fr: "Attention !!!"
-    },
-    won: {
-        en: "you got it",
-        fr: "Trouvé !"
-    }
+    quit_game: {en: "quit game", fr: "quitter le jeu"},
+    replay: {en: "Next puzzle", fr: "Prochaine Énigme"},
+    return_category: {en: "change category", fr: "changer de catégorie"},
+    return_home: {en: "return to main menu", fr: "retourner au menu principal"},
+    reveal: {en: "reveal", fr: "devoiler"},
+    reveal_answer: {en: "reveal answer", fr: "dévoiler la réponse"},
+    start: {en: "play now", fr: "lance-toi !"},
+    view: {en: "view", fr: "voir"},
+    warning: {en: "caution !!!", fr: "Attention !!!"},
+    won: {en: "you got it", fr: "Trouvé !"}
 });
